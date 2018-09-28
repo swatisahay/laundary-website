a893f2cbbab43fc96b581275b0d400d0ea40a953
Swati Sahay <swatiranjan0111@gmail.com>
2018-09-28 12:10:40 -0700

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,24 @@ import logo from '../assets/images/logo.jpg';
 function Header(){
   return (
     <div>
-      <nav className="navbar navbar-default bg-light">
-        <div className="navbar-header navbar-brand">
-          <Link to='/'> <img src={logo}/> </Link>
-        </div>
-        <ul className="nav navbar-nav">
-          <li className="nav-item"><Link className="nav-link" to='/home'>Home</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/aboutus'>About Us</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/sevices'>Services</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/schedule'>Schedule Pick-Up</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/contact'>Contact Us</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/login'>LogIn</Link></li>
-        </ul>
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+
+
+          <Link to='/' className="navbar-header navbar-brand"> <img src={logo}/> </Link>
+
+
+
+         <ul class="navbar-nav">
+            <li className="nav-item"><Link className="nav-link" to='/home'>Home</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/aboutus'>About Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/sevices'>Services</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/schedule'>Schedule Pick-Up</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/contact'>Contact Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/login'>LogIn</Link></li>
+          </ul>
       </nav>
+
+
       <style jsx>{`
         nav {
           border-bottom: 1px solid black;
@@ -24,13 +29,17 @@ function Header(){
         img{
           height: 100px;
           width: 100px;
+          float: left;
         }
         li {
-          display: inline-flex;
+          display: inline-block;
+
 
         }
         ul {
           list-style-type: none;
+          float: right;
+          padding-top: 30px;
         }
       `}</style>
     </div>
