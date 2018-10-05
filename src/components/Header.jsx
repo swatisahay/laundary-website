239ae6cebc52931414322a50581a82dc1ad17559
Swati Sahay <swatiranjan0111@gmail.com>
2018-10-05 15:13:23 -0700

    merge local with remote

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,31 +4,30 @@ import logo from '../assets/images/logo.jpg';
 function Header(){
   return (
     <div>
-      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
 
-        <Link to='/' className="navbar-header navbar-brand"> <img src={logo}/> </Link>
+          <Link to='/' className="navbar-header navbar-brand"> <img src={logo}/> </Link>
 
 
 
-        <ul className="navbar-nav">
-          <li className="nav-item"><Link className="nav-link" to='/'>Home</Link></li>
-          <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" to='/aboutus' id="navbardrop" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false">
+         <ul class="navbar-nav">
+            <li className="nav-item"><Link className="nav-link" to='/'>Home</Link></li>
+            <li clasName="nav-item dropdown">
+             <a className="nav-link dropdown-toggle" href='/aboutus' id="navbardrop" data-toggle="dropdown" role="button"  aria-haspopup="true" aria-expanded="false">
                About
-            </Link>
-            <div className="dropdown-menu">
-              <a className="dropdown-item" href='/pricing'>Pricing</a>
-              <a className="dropdown-item" href='/routes'>Routes</a>
+             </a>
+             <div className="dropdown-menu">
+               <Link className="dropdown-item" to='/pricing'>Pricing</Link>
+               <Link className="dropdown-item" to='/routes'>Routes</Link>
 
-            </div>
-          </li>
-          <li className="nav-item"><Link className="nav-link" to='/services'>Services</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/schedule'>Schedule Pick-Up</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/admin'>Admin</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/contact'>Contact Us</Link></li>
-          <li className="nav-item"><Link className="nav-link" to='/login'>LogIn</Link></li>
-        </ul>
+             </div>
+           </li>
+            <li className="nav-item"><Link className="nav-link" to='/services'>Services</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/schedule'>Schedule Pick-Up</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/contact'>Contact Us</Link></li>
+            <li className="nav-item"><Link className="nav-link" to='/login'>LogIn</Link></li>
+          </ul>
       </nav>
 
 
